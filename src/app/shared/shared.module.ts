a65bc646903182af68c6776821242a28f9031bc2
Jose Trinidad Garcia <internick33@gmail.com.com>
2023-08-18 18:46:36 -0600

    Fin seccion 7

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -12,7 +13,7 @@ import { HeaderComponent } from './header/header.component';
     SidebarComponent,
     HeaderComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [
     BreadcrumbsComponent,
     SidebarComponent,
