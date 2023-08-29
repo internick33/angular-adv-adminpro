941d931a743a6a9979564419dab184d11dd6d2ba
Jose Trinidad Garcia <internick33@gmail.com.com>
2023-08-29 16:36:33 -0600

    Fin seccion 8

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map  } from 'rxjs/operators';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,6 +9,30 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent {
+export class BreadcrumbsComponent implements OnDestroy{
+
+  public titulo: string = '';
+  public tituloSubs$: Subscription;
+
+  constructor( private router: Router, private route: ActivatedRoute){
+    
+    this.tituloSubs$ = this.getArgumentosRuta()
+    .subscribe( ({titulo}) => {
+      this.titulo = titulo;
+      document.title = `AdminPro - ${titulo}`;
+    });
+  }
+
+  getArgumentosRuta(){
+    return this.router.events.pipe(
+      filter(event => event instanceof ActivationEnd), 
+      filter((event: any) => event.snapshot.firstChild === null ),
+      map((event: any) => event.snapshot.data ),
+    )
+  }
+
+  ngOnDestroy(){
+    this.tituloSubs$.unsubscribe();
+  }
 
 }
